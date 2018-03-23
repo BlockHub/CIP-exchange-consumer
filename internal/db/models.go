eac142c475a769c33ac324049c0387a4a353afeb
AbeIgnocious <k.l.kubat@gmail.com>
2018-03-23 21:35:51 +0100

    add indexing to FK and time fields

--- internal/db/models.go
+++ internal/db/models.go
@@ -1,32 +1,32 @@
 package db
 
 type BitfinexOrderBook struct {
-	ID uint 		`gorm:"primary_key"`
-	MarketID uint
-	Time int64
+	ID uint 			`gorm:"primary_key"`
+	MarketID uint 		`gorm:"index"`
+	Time int64			`gorm:"index"`
 }
 
 type BitfinexOrder struct {
-	ID uint `gorm:"primary_key"`
-	OrderbookID uint
+	ID uint 			`gorm:"primary_key"`
+	OrderbookID uint 	`gorm:"index"`
 	Rate float64
 	//bitfinex supports giving the total number of sell/buyorders.
 	//however we should skimp on memory and not add those
 	//count int64
 	Quantity float64
-	Time int64
+	Time int64			`gorm:"index"`
 	}
 
 type BitfinexMarket struct {
-	ID uint `gorm:"primary_key"`
-	Ticker string	`gorm:"unique_index:idx_market"`
-	Quote string	`gorm:"unique_index:idx_market"`
+	ID uint 			`gorm:"primary_key"`
+	Ticker string		`gorm:"unique_index:idx_market"`
+	Quote string		`gorm:"unique_index:idx_market"`
 }
 
 type BitfinexTicker struct {
-	ID  uint `gorm:"primary_key"`
-	MarketID uint
+	ID  uint 			`gorm:"primary_key"`
+	MarketID uint		`gorm:"index"`
 	Price float64
 	Volume float64
-	Time int64
+	Time int64			`gorm:"index"`
 }
