481314e1db7752e0175a3adc2721ca42140f386b
AbeIgnocious <k.l.kubat@gmail.com>
2018-03-26 12:56:11 +0200

    change indexing for timescaledb

--- internal/db/models.go
+++ internal/db/models.go
@@ -5,7 +5,7 @@ import "time"
 type BitfinexOrderBook struct {
 	ID uint 			`gorm:"primary_key"`
 	MarketID uint 		`gorm:"index"`
-	Time time.Time			`gorm:"index"`
+	Time time.Time		`gorm:"primary_key"`
 }
 
 type BitfinexOrder struct {
@@ -16,7 +16,7 @@ type BitfinexOrder struct {
 	//however we should skimp on memory and not add those
 	//count int64
 	Quantity float64
-	Time time.Time		`gorm:"index"`
+	Time time.Time		`gorm:"primary_key"`
 	}
 
 type BitfinexMarket struct {
@@ -30,5 +30,5 @@ type BitfinexTicker struct {
 	MarketID uint		`gorm:"index"`
 	Price float64
 	Volume float64
-	Time time.Time		`gorm:"index"`
+	Time time.Time		`gorm:"primary_key"`
 }
